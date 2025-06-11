78336bb83eefc15c14c0b575906be964d58229c3
Bonzay993 <161145964+Bonzay993@users.noreply.github.com>
2025-06-11 14:19:16 +0000

    Moved the scripts and styling to their own locations

---
+++ static/scripts/script.js
@@ -0,0 +1,8 @@
+function showToast(message, duration = 3000) {
+  const toast = document.getElementById('toast');
+  toast.textContent = message;
+  toast.classList.add('show');
+  setTimeout(() => {
+    toast.classList.remove('show');
+  }, duration);
+}
